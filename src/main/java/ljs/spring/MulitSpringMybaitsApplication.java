f8e236306233748cfedbfe660dcca6c2a433b3ae
ljsyy <56251257+ljsyy@users.noreply.github.com>
2020-04-12 22:53:33 +0800

    完善

--- src/main/java/ljs/spring/MulitSpringMybaitsApplication.java
+++ src/main/java/ljs/spring/MulitSpringMybaitsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("ljs.spring.mapper.*.*")//将项目中对应的mapper类的路径加进来就可以了
 public class MulitSpringMybaitsApplication {
 
     public static void main(String[] args) {
